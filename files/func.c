478f44f14babf4076cb50b1631fb513f3d4ddedc
Swaminath <swaminath1991@gmail.com>
2016-01-30 11:36:25 -0700

    my func.c

---
+++ files/func.c
@@ -0,0 +1,21 @@
+#include "myheader.h"
+
+char reverse_string(char *str, int length)
+{
+	int i;
+	char temp;
+
+	if(str == '/0' || length == 0)
+	{
+		return (char)1;
+	}
+
+	for(i = 0;i < (length / 2);i++)
+	{
+		temp = str[i];
+		str[i] = str[(length - i) - 1];
+		str[(length - i) - 1] = temp;
+	}
+
+	return (char)0;
+}
